de84fff82a28b715a7cbec6b9b9acdd31c235936
chinmayvernekar <chinmay.vernekar49@gmail.com>
2023-08-19 08:34:12 +0200

    TEST-01

---
+++ src/main/java/com/chinmay/ci_cd/JavaCiCdExampleApplication.java
@@ -0,0 +1,13 @@
+package com.chinmay.ci_cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCiCdExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCiCdExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chinmay/ci_cd/JavaCiCdExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chinmay.ci_cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCiCdExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
