f79cd363e932d99a7ca8aa9efb655aa1c6a9ffe8
chinmayvernekar <chinmay.vernekar49@gmail.com>
2023-08-19 08:53:20 +0200

    02

--- src/main/java/com/chinmay/ci_cd/JavaCiCdExampleApplication.java
+++ src/main/java/com/chinmay/ci_cd/JavaCiCdExampleApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class JavaCiCdExampleApplication {
     
-    	@GetMapping
+    	@GetMapping 
     	public String HELLO() {
     	    return "HELLO GIT LAB!!!!!!!!";
     	} 
