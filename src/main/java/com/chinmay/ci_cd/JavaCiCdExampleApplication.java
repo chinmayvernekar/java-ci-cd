25f4fc180f9c16aa163f2c593c01db4b9e727c31
chinmayvernekar <chinmay.vernekar49@gmail.com>
2023-08-19 08:40:01 +0200

    API END POINT ADDED

--- src/main/java/com/chinmay/ci_cd/JavaCiCdExampleApplication.java
+++ src/main/java/com/chinmay/ci_cd/JavaCiCdExampleApplication.java
@@ -2,10 +2,21 @@ package com.chinmay.ci_cd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JavaCiCdExampleApplication {
-
+    
+    	@GetMapping
+    	public String HELLO() {
+    	    return "HELLO GIT LAB!!!!!!!!";
+    	}
+    
+    
+    
+    
 	public static void main(String[] args) {
 		SpringApplication.run(JavaCiCdExampleApplication.class, args);
 	}
